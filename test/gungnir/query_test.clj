e7ecc1b89325f00831710c70ff331f9499baf8cd
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2020-07-21 23:11:36 +0200

    Add q/find! tests

--- test/gungnir/query_test.clj
+++ test/gungnir/query_test.clj
@@ -1,12 +1,33 @@
 (ns gungnir.query-test
   (:require
    [clojure.test :refer :all]
+   [gungnir.query :as q]
+   [gungnir.core :refer [changeset]]
    [gungnir.test.util :as util]))
 
 (use-fixtures :once util/once-fixture)
 (use-fixtures :each util/each-fixture)
 
-(deftest test-find!)
+(def user-1-email "user@test.com")
+
+(def user-1
+  {:user/email user-1-email
+   :user/password user-1-email})
+
+(deftest test-find!
+  (testing "Find user by primary key"
+    (let [{:user/keys [id]} (-> user-1 changeset q/insert!)]
+      (is (= user-1-email (-> (q/find! :user id) :user/email)))))
+
+  ;; TODO fix failing tests
+  ;; (testing "Find user by primary key, automatic uuid translation"
+  ;;   (let [{:user/keys [id]} (-> user-1 changeset q/insert!)]
+  ;;     (is (= user-1-email (-> (q/find! :user (str id)) :user/email)))))
+
+  (testing "Find unknown user by primary key returns nil"
+    (is (nil? (-> (q/find! :user #uuid "1e626bf3-8fdf-4a66-b708-7aa35dafede9"))))
+    ;; (is (nil? (-> (q/find! :user "1e626bf3-8fdf-4a66-b708-7aa35dafede9"))))
+    ))
 
 (deftest test-find-by!)
 
