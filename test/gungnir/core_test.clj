3f8307593a031854c3cca959a909aa77479acf18
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2020-07-22 21:03:08 +0200

    Remove core test

--- test/gungnir/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gungnir.core-test
-  (:require
-   [clojure.test :refer :all]
-   [gungnir.test.util :as util]))
-
-(use-fixtures :once util/once-fixture)
-(use-fixtures :each util/each-fixture)
