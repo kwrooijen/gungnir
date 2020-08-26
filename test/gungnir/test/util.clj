00aa45d79557f10cf772a9198608ea688b92e53a
Kevin W. van Rooijen <kevin.van.rooijen@gmail.com>
2020-08-26 15:20:29 +0200

    Properly setup database for factory tests

--- test/gungnir/test/util.clj
+++ test/gungnir/test/util.clj
@@ -2,19 +2,32 @@
   "This namespace is used to setup a testing environment. This namespace
   should be included in all test namespaces. "
   (:require
+   [gungnir.database :refer [*database*]]
    [orchestra.spec.test :as st]
    [gungnir.test.util.database :as database]
    [gungnir.test.util.migrations :as migrations]
    [gungnir.test.util.model :as model]
    [next.jdbc]))
 
+(defn database-setup-once
+  ([]
+   (database/init!)
+   (database-setup-once *database*))
+  ([datasource]
+   (st/instrument)
+   (migrations/init! datasource)
+   (database/clear! datasource)
+   (model/init!)))
+
+(defn database-setup-each
+  ([] (database-setup-each *database*))
+  ([datasource]
+   (database/clear! datasource)))
+
 (defn once-fixture [tests]
-  (st/instrument)
-  (database/init!)
-  (migrations/init!)
-  (model/init!)
+  (database-setup-once)
   (tests))
 
 (defn each-fixture [tests]
-  (database/clear!)
+  (database-setup-each)
   (tests))
