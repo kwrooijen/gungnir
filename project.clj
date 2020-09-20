5cf005e792e40c340869cf75752e2507df5c16fe
Tommi Reiman <tommi@iki.fi>
2020-09-20 17:39:39 +0300

    update to latest (immutable) malli version

--- project.clj
+++ project.clj
@@ -6,21 +6,14 @@
   :dependencies [[seancorfield/next.jdbc "1.1.588"]
                  [org.postgresql/postgresql "42.2.16"]
                  [honeysql "1.0.444"]
-                 ;; For now we use lein-git-down to specifiy malli as a
-                 ;; dependency. We also need to add dynaload since lein-git-down
-                 ;; can't resolve it for some reason.
-                 [metosin/malli "a5bd5806d25fadb2f0025361189a641f50988df9"]
-                 [borkdude/dynaload "0.1.0"]
+                 [metosin/malli "0.0.1-20200920.124124-25"]
                  [differ "0.3.3"]
                  [hikari-cp "2.13.0"]
                  [kwrooijen/clj-database-url "0.0.1"]
                  [org.clojure/tools.logging "1.1.0"]]
   :plugins [[lein-cloverage "1.1.2"]
             [lein-codox "0.10.7"]
-            [lein-ancient "0.6.15"]
-            [reifyhealth/lein-git-down "0.3.7"]]
-
-  :middleware [lein-git-down.plugin/inject-properties]
+            [lein-ancient "0.6.15"]]
 
   :repositories [["public-github" {:url "git://github.com"}]
                  ["private-github" {:url "git://github.com" :protocol :ssh}]]
