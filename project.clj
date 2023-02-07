1b4b70a72082a89150d01a72e0dfb9037caf50ce
Kevin <kwrooijen@pm.me>
2023-02-07 08:37:55 +0100

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "MIT"}
   :source-paths ["src/clj" "src/cljc"]
   :dependencies [[com.github.seancorfield/next.jdbc "1.3.847"]
-                 [org.postgresql/postgresql "42.5.1"]
-                 [com.github.seancorfield/honeysql "2.4.969"]
+                 [org.postgresql/postgresql "42.5.3"]
+                 [com.github.seancorfield/honeysql "2.4.972"]
                  [metosin/malli "0.10.1"]
                  [differ "0.3.3"]
                  [hikari-cp "3.0.1"]
